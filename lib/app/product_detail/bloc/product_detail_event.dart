6bc96f713d1d4927ade079f51b7dce29851b9d46
mrdyman <andimardimansaputra@gmail.com>
2023-02-11 13:45:40 +0800

    finishing

--- lib/app/product_detail/bloc/product_detail_event.dart
+++ lib/app/product_detail/bloc/product_detail_event.dart
@@ -2,3 +2,8 @@ part of 'product_detail_bloc.dart';
 
 @immutable
 abstract class ProductDetailEvent {}
+
+class BuyProduct extends ProductDetailEvent {
+  final Product product;
+  BuyProduct(this.product);
+}
